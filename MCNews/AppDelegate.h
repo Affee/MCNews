89740df5e40491d1b47bc281464dc484858b6e32
Affee <304442951@qq.com>
2018-02-27 10:15:27 +0800

    Initial Commit

---
+++ MCNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCNews
+//
+//  Created by Affee on 2018/2/27.
+//  Copyright © 2018年 affee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
